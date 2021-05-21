d78b43dcdbf2f7f59d9cee104310e2a26fc1bab2
Vyacheslav Zayats <vz@Vyacheslavs-MacBook-Pro.local>
2021-05-21 23:52:23 +0300

    Add PostPage logic

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -2,19 +2,29 @@ import { Container, Grid, LinearProgress } from '@material-ui/core';
 import React, { useEffect } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getNewsThunk } from '../../redux/actions';
+import { getCommentsThunk, getSingleNewsByIdThunk } from '../../redux/actions';
+import { getNewsByIdSelector, newsSelector } from '../../redux/selectors';
 import { SinglePost } from '../../components/SinglePost';
 
 const PostPage = (props) => {
-    const { id: paramId } = useParams()
-    const { news } = useSelector((state) => state.mainState)
+    const { id: selectedId } = useParams()
+    const news = useSelector(newsSelector)
+    const kids = useSelector(getNewsByIdSelector(selectedId))
     const dispatch = useDispatch()
 
     useEffect(() => {
-        dispatch(getNewsThunk())
-    }, []);
+        if (!news.length) {
+            dispatch(getSingleNewsByIdThunk(selectedId))
+        }
+    }, [news])
 
-    if (!paramId) {
+    useEffect(() => {
+        if (kids) {
+            dispatch(getCommentsThunk(kids))
+        }
+    }, [kids]);
+
+    if (!selectedId) {
         return <Redirect to="/news" />
     }
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,7 +1,8 @@
 export const GET_IDS = "@main/GET_IDS";
 export const SET_NEWS = "@main/SET_NEWS";
 
-export const MAX_NEWS_COUNT = 100;
+export const MAX_NEWS_COUNT = 10;
+export const MAX_COMMENTS_COUNT = 10;
 
 // @main
 
@@ -43,25 +44,25 @@ export const getNewsThunk = () => async (dispatch) => {
     return dispatch(setNews(news));
   };
 
-  export const getCommentsThunk = () => async (dispatch) => {
-    const res = await fetch(
-      "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
-    );
-    const data = await res.json(); // [881238, 12838, 12838, 12838, 812388]
+  export const getCommentsThunk = (kids) => async (dispatch) => {
+    // [881238, 12838, 12838, 12838, 812388]
+    const filteredKids = kids.filter((_, id) => id < MAX_COMMENTS_COUNT);
   
-    if (!data?.length) {
-      return "setError";
-    }
-  
-    const filteredData = data.filter((_, id) => id < MAX_NEWS_COUNT);
-  
-    const news = await Promise.all(
-      filteredData.map((id) =>
+    const comments = await Promise.all(
+      filteredKids.map((id) =>
         fetch(
           `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
         )
       )
     ).then((results) => Promise.all(results.map((r) => r.json())));
 
-    return dispatch(setNews(news));
+    console.log('comments', comments)
+
+    return 123;
   };
+
+export const getSingleNewsByIdThunk = (selectedId) => async (dispatch) => {
+  fetch(`https://hacker-news.firebaseio.com/v0/item/${selectedId}.json?print=pretty`)
+    .then(res => res.json())
+    .then(data => dispatch(setNews([data])))
+}
